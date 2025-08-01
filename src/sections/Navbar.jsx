2cacf7ad5a647c16dd984228e9275ac01b9cdf02
nishantsingh604 <nishantrohilla28@gmail.com>
2025-08-01 23:36:42 +0530

    started with the page and navbar

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed inset-x-0 z-20 w-full backdrop-blur-lg bg-primary/40'>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
